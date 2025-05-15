9162b6e8de91f57c39916b088e3aaebe825ec285
Mostapha <mostapha-bachir.addi@redstoneformations.fr>
2025-05-15 10:58:20 +0200

    exemple criteria

--- hibernate-projet/src/main/java/com/exemple/models/HQLDemo.java
+++ hibernate-projet/src/main/java/com/exemple/models/HQLDemo.java
@@ -6,10 +6,16 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.sqm.tree.from.SqmRoot;
+
+import com.google.protobuf.RpcCallback;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 @Entity
 public class HQLDemo {
@@ -58,21 +64,50 @@ public class HQLDemo {
 
     public static final SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(HQLDemo.class).buildSessionFactory();
 
+    // public static List<HQLDemo> fetchAll() {
+    //     try (Session s = sessionFactory.openSession()) {
+    //         String hql = "FROM HQLDemo";
+    //         return s.createQuery(hql, HQLDemo.class).list();
+    //     }
+    // }
+
+    // public static List<HQLDemo> fetchWithMinScore(int min) {
+    //     try (Session s = sessionFactory.openSession()) {
+    //         String hql = "FROM HQLDemo WHERE score >= :param";
+    //         return s.createQuery(hql, HQLDemo.class)
+    //                 .setParameter("param", min)
+    //                 .list();
+    //     }
+    // }
+
+
+    // Using Criteria API
     public static List<HQLDemo> fetchAll() {
         try (Session s = sessionFactory.openSession()) {
-            String hql = "FROM HQLDemo";
-            return s.createQuery(hql, HQLDemo.class).list();
+            CriteriaBuilder cb = s.getCriteriaBuilder();
+            CriteriaQuery<HQLDemo> cq = cb.createQuery(HQLDemo.class);
+            var root = cq.from(HQLDemo.class);
+
+            cq.select(root);
+
+            return s.createQuery(cq).getResultList();
         }
     }
 
     public static List<HQLDemo> fetchWithMinScore(int min) {
         try (Session s = sessionFactory.openSession()) {
-            String hql = "FROM HQLDemo WHERE score >= :param";
-            return s.createQuery(hql, HQLDemo.class)
-                    .setParameter("param", min)
-                    .list();
+            CriteriaBuilder cb = s.getCriteriaBuilder();
+            CriteriaQuery<HQLDemo> cq = cb.createQuery(HQLDemo.class);
+            Root<HQLDemo> root = cq.from(HQLDemo.class);
+            cq.select(root)
+                .where(cb.gt(root.get("score"), min));
+            
+            return s.createQuery(cq).getResultList();
         }
+
+        
     }
+
 
     @Override
     public String toString() {
